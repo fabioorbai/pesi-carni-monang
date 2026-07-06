f2eac34f8920dae5a5cb808e4b589d363d98e31a
fabioorbai <fabio.orbai@gmail.com>
2026-07-06 06:21:50 +0200

    Add files via upload

--- pesi-carni-monang/config.js
+++ pesi-carni-monang/config.js
@@ -1,3 +1,8 @@
-// Per ora l'app funziona in modalità DEMO OFFLINE.
-// Più avanti qui inseriremo i dati Firebase per sincronizzare griglia e cassa in tempo reale.
-window.MONANG_FIREBASE_CONFIG = null;
+window.firebaseConfig = {
+  apiKey: "AIzaSyAfTggVVuS4atu0-k9ogfxexkUIGhY20J8",
+  authDomain: "pesi-carni-monang.firebaseapp.com",
+  projectId: "pesi-carni-monang",
+  storageBucket: "pesi-carni-monang.firebasestorage.app",
+  messagingSenderId: "957161248371",
+  appId: "1:957161248371:web:7e11ab9dedf84563073c06"
+};
